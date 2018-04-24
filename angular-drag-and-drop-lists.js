147eb60dfaf0ff1ad085351d55dc00a87ff5aa76
Kevin Eshleman <hello@redkevin.com>
2018-04-24 14:59:13 -0500

    updated version to 2.1.4

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -1,10 +1,9 @@
 /**
- * angular-drag-and-drop-lists v2.1.3
+ * angular-drag-and-drop-lists v2.1.4
  *
  * Copyright (c) 2014 Marcel Juenemann marcel@juenemann.cc
  * Copyright (c) 2014-2017 Google Inc.
  * https://github.com/marceljuenemann/angular-drag-and-drop-lists
- * Modified work at https://github.com/marceljuenemann/angular-drag-and-drop-lists
  *
  * License: MIT
  */
