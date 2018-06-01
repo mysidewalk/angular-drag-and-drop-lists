bdd9415699775e92c1c21f6595b79e0f8bb2b415
Kevin Eshleman <hello@redkevin.com>
2018-05-31 22:19:14 -0500

    upped version number to v2.1.0-mysw.3

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -1,5 +1,5 @@
 /**
- * angular-drag-and-drop-lists v2.1.0-mysw.2
+ * angular-drag-and-drop-lists v2.1.0-mysw.3
  *
  * Copyright (c) 2014 Marcel Juenemann marcel@juenemann.cc
  * Copyright (c) 2014-2017 Google Inc.
